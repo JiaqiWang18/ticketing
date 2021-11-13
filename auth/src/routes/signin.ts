d8a095cad2819495ddb29b6ec43c83694d47b7f5
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-11-12 23:25:07 -0500

    finish signin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 import { body } from 'express-validator';
+import { BadRequestError } from '../error/bad-request-error';
 import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/users';
+import { Password } from '../services/password';
 
 const router = express.Router();
 
@@ -14,8 +18,26 @@ router.post(
       .withMessage('You must supply a password'),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordMatch) {
+      throw new BadRequestError('Invalid credentials');
+    }
+    // generate jwt
+    const userJwt = jwt.sign(
+      { id: existingUser.id, email: existingUser.email },
+      process.env.JWT_KEY!
+    );
+    req.session = { jwt: userJwt };
+    res.status(200).send(existingUser);
   }
 );
 
