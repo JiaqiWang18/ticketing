f66c6d3a0c447f36bb3936ef2bfc47d3bbadf498
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-11-12 21:45:57 -0500

    generate jwt

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import { BadRequestError } from '../error/bad-request-error';
 import { RequestValidationError } from '../error/request-validation-error';
 import { User } from '../models/users';
@@ -31,6 +32,9 @@ router.post(
     const user = User.build({ email, password });
     await user.save();
 
+    // generate jwt
+    const userJwt = jwt.sign({ id: user.id, email: user.email }, 'asdf');
+    req.session = { jwt: userJwt };
     res.status(201).send(user);
   }
 );
