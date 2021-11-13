fe2d2a895dda34e8080127adfb058f6100eee3a4
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-11-12 23:04:12 -0500

    create signin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,25 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../error/request-validation-error';
 
 const router = express.Router();
 
-router.post('/api/users/sigin', (req, res) => {
-  res.send('Hello There');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+    const { email, password } = req.body;
+  }
+);
 
 export { router as signinRoouter };
