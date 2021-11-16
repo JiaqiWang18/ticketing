34414b99e74817d169501debc14b1ecd3304e92c
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-11-15 22:55:02 -0500

    create API

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,16 @@
-export default () => {
-  return <h1>Landing Page!</h1>;
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser');
+  console.log(currentUser);
+
+  return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get('/api/users/currentuser');
+  return data;
+};
+
+export default LandingPage;
