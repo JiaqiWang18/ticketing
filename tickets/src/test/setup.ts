7d255fe3dde4aca133b598322eeaca85a234f86f
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-11-22 01:24:36 -0500

    refactor

---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,47 @@
+import request from 'supertest';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+
+let mongo: any;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'test_jwt';
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  // empty db
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+// global sign in method to make it easy to auth
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+  const authResponse = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+  const cookie = authResponse.get('Set-Cookie');
+  return cookie;
+};
