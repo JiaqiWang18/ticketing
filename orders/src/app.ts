84eb974f1f23fe94dd20773e051329c9a7ca30b7
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-11-26 00:40:15 -0500

    skaffold routes

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -2,10 +2,10 @@ import express from 'express';
 import 'express-async-errors';
 import cookieSession from 'cookie-session';
 import { NotFoundError, currentUser, errorHandler } from '@jwmodules/common';
-import { createTicketRouter } from './routes/new';
-import { showTicketRouter } from './routes/show';
-import { indexTicketRouter } from './routes';
-import { updateTicketRouter } from './routes/update';
+import { indexOrderRouter } from './routes';
+import { deleteOrderRouter } from './routes/delete';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true); // trust ingress nginx proxy
@@ -17,10 +17,10 @@ app.use(
   })
 );
 app.use(currentUser);
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
