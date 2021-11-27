4468f592437d0edb2625c34599c1bd3e66ce1072
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-11-26 20:50:01 -0500

    publish order cancelled event

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -5,7 +5,9 @@ import {
   requireAuth,
 } from '@jwmodules/common';
 import express, { Request, Response } from 'express';
+import { OrderCancelledPublisher } from '../../events/publishers/order-cancelled-publisher';
 import { Order } from '../models/order';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -22,6 +24,13 @@ router.delete(
     }
     order.status = OrderStatus.Cancelled;
     await order.save();
+    // publish order cancelled event
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
     res.status(204).send(order);
   }
 );
