396c9535f736a1542233c59e16106ab9821d9987
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-12-03 22:25:06 -0500

    add event listener

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 import { app } from './app';
+import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
 import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
@@ -31,6 +33,10 @@ const start = async () => {
     // gracefully close connection
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
+
+    // wire up listeners
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
